f8a2d03050669ddeb21fd72da7bc65e518c6e7e8
Jace Babin <jbabin91@gmail.com>
2024-08-31 12:43:57 -0700

    feat: :sparkles: add dependencies for testing

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,35 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
+  build: {
+    rollupOptions: {
+      external: ['fsevents'],
+      output: {
+        experimentalMinChunkSize: 3500,
+      },
+    },
+  },
+  optimizeDeps: { exclude: ['fsevents'] },
   plugins: [react(), tsconfigPaths()],
+  preview: {
+    port: 3000,
+  },
+  server: {
+    port: 3000,
+  },
+  test: {
+    coverage: {
+      include: ['src/**'],
+    },
+    environment: 'jsdom',
+    exclude: ['**/node_modules/**', '**/e2e/**'],
+    globals: true,
+    setupFiles: './src/testing/setup-tests.ts',
+  },
 });
